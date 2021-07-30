7f716965010b30c9a22e621bbd15dc2b2d7d9cf2
Alena DENIS <basstet067@yahoo.com>
2021-07-30 20:06:42 +0200

    refactor(script.js): used split to create an array

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 'use strict';
 
-let num = 266219,
-    numArray = [],
-    numMult;
-
-num = num.toString(); // преобразую в строку, чтобы достать цифры по-отдельности
- 
-for (let i = 0; i < 6; i++) {
-  numArray.push(+num[i]); // заполняю массив цифрами, преобразуя их в числа
-}
-numMult = numArray.reduce((accumulator, currentValue) => accumulator * currentValue); // перемножаю
+const num = 266219,
+      numArray = num.toString().split('');
+      
+let numMult = numArray.reduce((accumulator, currentValue) => accumulator * currentValue); // перемножаю
 
 console.log(numMult);
 
