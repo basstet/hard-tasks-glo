22b275d69ab00957f04c3a7ba6d7dd34a79b842d
Alena DENIS <basstet067@yahoo.com>
2021-08-02 17:41:52 +0200

    feat(script.js): tasks of lesson 3

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Задания урока 2:
 const num = 266219,
       numArray = num.toString().split('');
       
@@ -10,3 +11,37 @@ console.log(numMult);
 numMult **= 3; // возвожу в 3 степень
 
 console.log(numMult.toString().substr(0, 2)); // преобразую в строку и вывожу первые две цифры полученного числа
+
+// Урок 3, задание 1:
+let lang = 'ru',
+  weekDays1 = [],
+  weekDays2 = [],
+  weekDays3 = new Map();
+
+if (lang === 'ru') {
+  weekDays1 = ['пн','вт','ср','чт','пт','сб','вс'];
+} else {
+  weekDays1 = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
+}
+console.log(weekDays1);
+
+switch (lang) {
+  case 'ru':
+    weekDays2 = ['пн','вт','ср','чт','пт','сб','вс'];
+    break;
+  case 'en':
+  default:
+    weekDays2 = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
+    break;
+}
+console.log(weekDays2);
+
+weekDays3 = new Map([
+  ['ru', ['пн','вт','ср','чт','пт','сб','вс']],
+  ['en', ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']]
+]);
+console.log(weekDays3.get(lang));
+
+// Урок 3, задание 2:
+let namePerson,
+  personStatus = namePerson === `Артем` ? console.log(`директор`) : namePerson === `Максим` ? console.log(`преподаватель`) : console.log(`студент`);
