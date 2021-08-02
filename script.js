7688144b7ddcd8fe0f17653d54e6c9ce8980aaaf
Alena DENIS <basstet067@yahoo.com>
2021-08-02 18:38:31 +0200

    refactor(script.js): code of lesson 2 is deleted, redused size of code

--- script.js
+++ script.js
@@ -1,46 +1,36 @@
 'use strict';
 
-// Задания урока 2:
-const num = 266219,
-      numArray = num.toString().split('');
-      
-let numMult = numArray.reduce((accumulator, currentValue) => accumulator * currentValue); // перемножаю
-
-console.log(numMult);
-
-numMult **= 3; // возвожу в 3 степень
-
-console.log(numMult.toString().substr(0, 2)); // преобразую в строку и вывожу первые две цифры полученного числа
-
 // Урок 3, задание 1:
 let lang = 'ru',
-  weekDays1 = [],
-  weekDays2 = [],
-  weekDays3 = new Map();
-
-if (lang === 'ru') {
-  weekDays1 = ['пн','вт','ср','чт','пт','сб','вс'];
-} else {
-  weekDays1 = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
-}
-console.log(weekDays1);
-
-switch (lang) {
-  case 'ru':
-    weekDays2 = ['пн','вт','ср','чт','пт','сб','вс'];
-    break;
-  case 'en':
-  default:
-    weekDays2 = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
-    break;
-}
-console.log(weekDays2);
-
-weekDays3 = new Map([
+  weekDays;
+const weekDaysLang = new Map([
   ['ru', ['пн','вт','ср','чт','пт','сб','вс']],
   ['en', ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']]
 ]);
-console.log(weekDays3.get(lang));
+
+// 1a:
+if (lang === 'ru') {
+  weekDays = weekDaysLang.get('ru');
+} else {
+  weekDays = weekDaysLang.get('en');
+}
+console.log(weekDays);
+
+// 1b:
+switch (lang) {
+  case 'ru':
+    weekDays = weekDaysLang.get('ru');
+    break;
+  case 'en':
+  default:
+    weekDays = weekDaysLang.get('en');
+    break;
+}
+console.log(weekDays);
+
+// 1c:
+weekDays = weekDaysLang.get(lang);
+console.log(weekDays);
 
 // Урок 3, задание 2:
 let namePerson,
