0f888cdc2d95b1aa113ad5b4d5d1b6fdff5eb1b5
Alena DENIS <basstet067@yahoo.com>
2021-08-03 19:10:15 +0200

    feat(script.js): task for lesson 4

--- script.js
+++ script.js
@@ -1,37 +1,17 @@
 'use strict';
 
-// Урок 3, задание 1:
-let lang = 'ru',
-  weekDays;
-const weekDaysLang = new Map([
-  ['ru', ['пн','вт','ср','чт','пт','сб','вс']],
-  ['en', ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']]
-]);
+const cutText = function(textToCut) {
+  if (typeof textToCut === 'string') {
+    if (textToCut.length > 30) {
+      // удаляем пробелы, обрезаем текст и добавляем многоточие:
+      return textToCut.trim().substr(0, 30).padEnd(33, '.');
+    } else {
+      // удаляем пробелы:
+      return textToCut.trim();
+    }
+  } else {
+    alert(`Значение не является текстом`);
+  }
+};
 
-// 1a:
-if (lang === 'ru') {
-  weekDays = weekDaysLang.get('ru');
-} else {
-  weekDays = weekDaysLang.get('en');
-}
-console.log(weekDays);
-
-// 1b:
-switch (lang) {
-  case 'ru':
-    weekDays = weekDaysLang.get('ru');
-    break;
-  case 'en':
-  default:
-    weekDays = weekDaysLang.get('en');
-    break;
-}
-console.log(weekDays);
-
-// 1c:
-weekDays = weekDaysLang.get(lang);
-console.log(weekDays);
-
-// Урок 3, задание 2:
-let namePerson,
-  personStatus = namePerson === `Артем` ? console.log(`директор`) : namePerson === `Максим` ? console.log(`преподаватель`) : console.log(`студент`);
+alert(cutText(prompt(`Введите текст`)));
