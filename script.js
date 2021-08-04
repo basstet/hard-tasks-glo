43077e08b671eb606fca8cc5917b9b3b5431ad5d
Alena DENIS <basstet067@yahoo.com>
2021-08-04 18:15:35 +0200

    feat(script.js): tasks for lesson 5

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 'use strict';
 
-const cutText = function(textToCut) {
-  if (typeof textToCut === 'string') {
-    if (textToCut.trim().length > 30) {
-      // удаляем пробелы, обрезаем текст и добавляем многоточие:
-      return textToCut.trim().substr(0, 30) + `...`;
-    } else {
-      // удаляем пробелы:
-      return textToCut.trim();
-    }
-  } else {
-    alert(`Значение не является текстом`);
-  }
-};
+const arr = [];
 
-alert(cutText(prompt(`Введите текст`)));
+// заполняем массив путем ввода чисел
+for (let i = 0; i < 7; i++) {
+  arr[i] = prompt(`Число ${i+1}`);
+}
+console.log(arr);
+
+// // определяем какие из них начинаются на 2 или 4
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i][0] === '2' ||  arr[i][0] === '4') {
+    console.log(arr[i]);
+  }
+}
+
+// простые числа
+let hasRemainder;
+for (let i = 1; i <= 100; i++) {
+  for (let j = 2; j <= 100; j++) {
+    hasRemainder = i % j;
+    if (!hasRemainder && i !== j) {
+      break;
+    } else if (!hasRemainder && i === j) {
+      console.log(`${i} делители этого числа 1 и ${j}`);
+    }
+  }
+}
