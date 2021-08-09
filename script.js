8109118534acf0338e31ad5c5cdb69a76a290aa2
Alena DENIS <basstet067@yahoo.com>
2021-08-09 22:46:16 +0200

    feat(script.js): task date format

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 'use strict';
 
-const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-const today = new Date().toLocaleString('ru', {weekday: 'long'});
-console.log(today);
+const p1 = document.createElement('p');
+p1.className = 'date-a';
+document.body.appendChild(p1);
+const todayDate1 = new Date().toLocaleString('ru', {
+  weekday: 'long',
+  day: 'numeric',
+  month: 'long',
+  year: 'numeric',
+});
 
-const ul = document.createElement('ul');
-document.querySelector('body').appendChild(ul);
+const p2 = document.createElement('p');
+p2.className = 'date-b';
+document.body.appendChild(p2);
 
-for (let item of week) {
-  const text = document.createTextNode(item),
-        li = document.createElement('li');
-  document.querySelector('ul').appendChild(li).appendChild(text);
-  if (item === 'Суббота' || item === 'Воскресенье') {
-    li.style.fontStyle = 'italic';
-  }
-  if (item.toLowerCase() === today) {
-    li.style.fontWeight = 'bold';
-  }
-}
+let conjHour,
+    conjMin,
+    conjSec;
+
+const conjTime = function(h, m, s) {
+  conjHour = (+h.slice(-1) === 1 && +h !== 11) ? 'час' : 
+    ((+h.slice(-1) > 1) && (+h.slice(-1) < 5) && (+h < 11 || +h > 14)) ? 'часа' : 
+    'часов';
+  conjMin = (+m.slice(-1) === 1 && +m !== 11) ? 'минута' : 
+    ((+m.slice(-1) > 1) && (+m.slice(-1) < 5) && (+s < 11 || +m > 14)) ? 'минуты' : 
+    'минут';
+  conjSec = (+s.slice(-1) === 1 && +s !== 11) ? 'секунда' : 
+    ((+s.slice(-1) > 1) && (+s.slice(-1) < 5) && (+s < 11 || +s > 14)) ? 'секунды' : 
+    'секунд';
+};
+
+const updateTime = setInterval(function() {
+  const todayHour = new Date().toLocaleString('ru', {hour: 'numeric'});
+  const todayMin = new Date().toLocaleString('ru', {minute: 'numeric'});
+  const todaySec = new Date().toLocaleString('ru', {second: 'numeric'});
+  conjTime(todayHour, todayMin, todaySec);
+
+  p1.innerHTML = `Сегодня ${todayDate1}, ${todayHour} ${conjHour} ${todayMin} ${conjMin} ${todaySec} ${conjSec}`;
+
+  const today2 = new Date().toLocaleString('ru');
+  p2.innerHTML = today2;
+}, 1000);
