c10504cf2c064740087ce8cd647934f34930607e
Alena DENIS <basstet067@yahoo.com>
2021-08-14 21:24:44 +0200

    feat(index.html, style.css, script.js): random colors btn

--- script.js
+++ script.js
@@ -1,43 +1,16 @@
 'use strict';
 
-const p1 = document.createElement('p');
-p1.className = 'date-a';
-document.body.appendChild(p1);
-const todayDate1 = new Date().toLocaleString('ru', {
-  weekday: 'long',
-  day: 'numeric',
-  month: 'long',
-  year: 'numeric',
+const titleColor = document.querySelector('.color'),
+      changeBtn = document.querySelector('#change'),
+      rangeStr = `0123456789ABCDEF`;
+
+changeBtn.addEventListener('click', function() {
+  let newColor = [];
+  for (let i = 0; i < 6; i++) {
+    newColor += rangeStr[Math.floor(Math.random() * 16)];
+  }
+
+  changeBtn.style.color = `#${newColor}`;
+  titleColor.textContent = `#${newColor}`;
+  document.body.style.backgroundColor = `#${newColor}`;
 });
-
-const p2 = document.createElement('p');
-p2.className = 'date-b';
-document.body.appendChild(p2);
-
-let conjHour,
-    conjMin,
-    conjSec;
-
-const conjTime = function(h, m, s) {
-  conjHour = (+h.slice(-1) === 1 && +h !== 11) ? 'час' : 
-    ((+h.slice(-1) > 1) && (+h.slice(-1) < 5) && (+h < 11 || +h > 14)) ? 'часа' : 
-    'часов';
-  conjMin = (+m.slice(-1) === 1 && +m !== 11) ? 'минута' : 
-    ((+m.slice(-1) > 1) && (+m.slice(-1) < 5) && (+s < 11 || +m > 14)) ? 'минуты' : 
-    'минут';
-  conjSec = (+s.slice(-1) === 1 && +s !== 11) ? 'секунда' : 
-    ((+s.slice(-1) > 1) && (+s.slice(-1) < 5) && (+s < 11 || +s > 14)) ? 'секунды' : 
-    'секунд';
-};
-
-const updateTime = setInterval(function() {
-  const todayHour = new Date().toLocaleString('ru', {hour: 'numeric'});
-  const todayMin = new Date().toLocaleString('ru', {minute: 'numeric'});
-  const todaySec = new Date().toLocaleString('ru', {second: 'numeric'});
-  conjTime(todayHour, todayMin, todaySec);
-
-  p1.innerHTML = `Сегодня ${todayDate1}, ${todayHour} ${conjHour} ${todayMin} ${conjMin} ${todaySec} ${conjSec}`;
-
-  const today2 = new Date().toLocaleString('ru');
-  p2.innerHTML = today2;
-}, 1000);
